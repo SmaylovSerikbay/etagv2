d1ee0c2fc40e94a48266d7277ab694968ffc1991
SmaylovSerikbay <augmented.vr@gmail.com>
2025-02-11 13:25:39 +0500

    fix

--- etag/urls.py
+++ etag/urls.py
@@ -4,6 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from profiles import views
 from django.contrib.auth import views as auth_views
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse("healthy")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,6 +18,9 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='profiles/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='welcome'), name='logout'),
     path('signup/', views.signup, name='signup'),
+    
+    # Health check
+    path('health/', health_check, name='health_check'),
 ]
 
 if settings.DEBUG:
