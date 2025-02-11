dee80cb25e0c53878c392cc11a8e388a808d8faa
SmaylovSerikbay <augmented.vr@gmail.com>
2025-02-11 13:10:35 +0500

    fix

--- etag/settings/production.py
+++ etag/settings/production.py
@@ -7,28 +7,31 @@ DEBUG = True  # Временно включим DEBUG для просмотра 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['http://my-business-card.kz', 'https://my-business-card.kz']
 
 # Database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': 'etag',
+        'USER': 'etag',
+        'PASSWORD': 'etag',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = os.getenv('STATIC_URL', '/static/')
-STATIC_ROOT = os.getenv('STATIC_ROOT', os.path.join(BASE_DIR, 'static'))
+STATIC_URL = '/static/'
+STATIC_ROOT = '/app/static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'profiles/static'),
+]
 
 # Media files
-MEDIA_URL = os.getenv('MEDIA_URL', '/media/')
-MEDIA_ROOT = os.getenv('MEDIA_ROOT', os.path.join(BASE_DIR, 'media'))
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/app/media'
 
 # Security settings
 SECURE_SSL_REDIRECT = False
@@ -68,11 +71,6 @@ LOGGING = {
             'propagate': True,
         },
         'django.request': {
-            'handlers': ['console', 'file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-        'django.db.backends': {
             'handlers': ['console', 'file'],
             'level': 'DEBUG',
             'propagate': True,
