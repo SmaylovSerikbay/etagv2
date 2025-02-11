eb052c21f742f344e5e908f236cc70aff96b891e
SmaylovSerikbay <augmented.vr@gmail.com>
2025-02-11 13:00:25 +0500

    fix all

--- etag/urls.py
+++ etag/urls.py
@@ -3,11 +3,17 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from profiles import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.welcome, name='welcome'),  # Корневой URL
     path('profiles/', include('profiles.urls')),  # URL для profiles
+    
+    # Аутентификация
+    path('login/', auth_views.LoginView.as_view(template_name='profiles/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='welcome'), name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
 
 if settings.DEBUG:
